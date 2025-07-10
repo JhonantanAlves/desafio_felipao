07fe92a59a449d53ce61a28203fec6cd1d615a2f
Jhonantan Alves <jhonantancbfsa@gmail.com>
2025-07-09 22:58:59 -0300

    Estrutura de condição adicionada

--- classificadorHeroi.js
+++ classificadorHeroi.js
@@ -4,12 +4,24 @@ let experiencia = document.getElementById("exp");
 
 let botao = document.getElementById("calcular");
 
-botao.addEventListener("click", () => {
+botao.addEventListener("click", (event) => {
   event.preventDefault();
-  let resultadoHeroi = heroi.value;
-  let resultadoExperiencia = experiencia.value;
-  console.log(resultadoHeroi, resultadoExperiencia);
+  let nomeDoHeroi = heroi.value;
+  let experienciaNumerica = parseInt(experiencia.value);
 
+  if (experienciaNumerica <= 1000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível ferro`;
+      resultadoFinal.appendChild(div);
+    } else {
+      alert("Elemento inválido!");
+    }
+  } else {
+    alert("Preencha o campo");
+  }
   document.getElementById("heroi").value = "";
   document.getElementById("exp").value = "";
 });
