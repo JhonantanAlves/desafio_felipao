12c6850b60caf71d369f1e3eeec530c8b9b32d31
Jhonantan Alves <jhonantancbfsa@gmail.com>
2025-07-10 21:37:11 -0300

    Adicionado as condições

--- classificadorHeroi.js
+++ classificadorHeroi.js
@@ -14,13 +14,67 @@ botao.addEventListener("click", (event) => {
 
     if (resultadoFinal) {
       let div = document.createElement("p");
-      div.textContent = `O herói ${nomeDoHeroi} está no nível ferro`;
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Ferro!`;
       resultadoFinal.appendChild(div);
-    } else {
-      alert("Elemento inválido!");
+    }
+  } else if (experienciaNumerica >= 1001 && experienciaNumerica <= 2000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Bronze!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica >= 2001 && experienciaNumerica <= 5000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Prata!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica >= 5001 && experienciaNumerica <= 7000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Ouro!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica >= 7001 && experienciaNumerica <= 8000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Platina!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica >= 8001 && experienciaNumerica <= 9000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Ascendente!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica >= 9001 && experienciaNumerica <= 10000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Imortal!`;
+      resultadoFinal.appendChild(div);
+    }
+  } else if (experienciaNumerica > 10000) {
+    let resultadoFinal = document.getElementById("resultado");
+
+    if (resultadoFinal) {
+      let div = document.createElement("p");
+      div.textContent = `O herói ${nomeDoHeroi} está no nível Radiante!`;
+      resultadoFinal.appendChild(div);
     }
   } else {
-    alert("Preencha o campo");
+    alert("Preencha o campo corretamente");
   }
   document.getElementById("heroi").value = "";
   document.getElementById("exp").value = "";
